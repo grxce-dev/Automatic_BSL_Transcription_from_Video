2300d6ab6412d102602d7d72df81d438e0232623
Grace <129192407+Gunit-12@users.noreply.github.com>
2026-03-04 21:02:02 +0000

    EOD save

---
+++ notebook/notes.md
@@ -0,0 +1,9 @@
+task:
+need to redo BAD signs - to many zeros?
+accuracy to good? - find a solution
+need some diversity in the signers somehow - incl. lighting, background, top, ect
+
+Not sure: but i think that it might not be measuring the hand landmarks correctly? like the actual positioning... for example BAD and GOOD both have closed fists but one has thumb up and the other has pinky up but still getting classified wrong 
+
+reread about live captioning 
+- work on location and creating actual captions 
---
+++ src/live_captioning.py
@@ -0,0 +1,116 @@
+
+import os
+import cv2
+import numpy as np
+import mediapipe as mp
+from tensorflow import keras
+from collections import deque, Counter
+from keras.models import load_model
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
+
+SEQUENCE_LENGTH = 35
+NUM_FEATURES = 126
+DATA_PATH = "data"
+MODEL_PATH = "models/bsl_multiclass_model.h5"
+
+CONFIDENCE_THRESHOLD =  0.0 # 0.80
+SMOOTHING_WINDOW =  1 # 5
+
+model = load_model(MODEL_PATH)
+
+class_names = sorted([
+    folder for folder in os.listdir(DATA_PATH)
+    if os.path.isdir(os.path.join(DATA_PATH, folder))
+])
+
+print("Loaded classes:", class_names)
+
+
+base_options = python.BaseOptions(
+    model_asset_path="data/hand_landmarker.task"
+)
+
+options = vision.HandLandmarkerOptions(
+    base_options=base_options,
+    num_hands=2
+)
+
+detector = vision.HandLandmarker.create_from_options(options)
+
+sequence = []
+prediction_history = deque(maxlen=SMOOTHING_WINDOW)
+
+# START WEBCAM
+cap = cv2.VideoCapture(0)
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    mp_image = mp.Image(
+        image_format=mp.ImageFormat.SRGB,
+        data=rgb
+    )
+
+    result = detector.detect(mp_image)
+    frame_features = []
+
+    if result.hand_landmarks:
+        for hand in result.hand_landmarks:
+            for landmark in hand:
+                frame_features.extend([landmark.x, landmark.y, landmark.z])
+
+    # Pad to fixed size (126 features)
+    while len(frame_features) < NUM_FEATURES:
+        frame_features.append(0)
+
+    sequence.append(frame_features)
+
+    if len(sequence) > SEQUENCE_LENGTH:
+        sequence.pop(0)
+
+    display_text = "..."
+
+
+    # PREDICT WHEN WINDOW FULL
+
+    if len(sequence) == SEQUENCE_LENGTH:
+
+        input_data = np.array(sequence)
+
+        # Standardise (same as training)
+        mean = np.mean(input_data)
+        std = np.std(input_data) + 1e-8
+        input_data = (input_data - mean) / std
+
+        input_data = np.expand_dims(input_data, axis=0)
+
+        prediction = model.predict(input_data, verbose=0)
+        predicted_index = np.argmax(prediction)
+        confidence = prediction[0][predicted_index]
+
+        if confidence > CONFIDENCE_THRESHOLD:
+            prediction_history.append(predicted_index)
+
+        if len(prediction_history) > 0:
+            most_common = Counter(prediction_history).most_common(1)[0][0]
+            display_text = f"{class_names[most_common]} ({confidence:.2f})"
+
+    # DISPLAY RESULT
+    cv2.putText(frame,
+                display_text,
+                (20, 50),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                1,
+                (0, 255, 0),
+                2)
+
+    cv2.imshow("BSL Live Captioning", frame)
+
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
