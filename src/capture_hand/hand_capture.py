2984ee0ffd21260b8ad28321140bab8abd14c811
Grace <129192407+Gunit-12@users.noreply.github.com>
2026-04-21 00:52:04 +0100

    more data <3

--- src/capture_hand/hand_capture.py
+++ src/capture_hand/hand_capture.py
@@ -14,7 +14,7 @@ sequence = []
 recording = False
 
 # Save frames into .npy file in folder of choice
-save_folder = "data/hand/1"
+save_folder = "data/hand/6"
 os.makedirs(save_folder, exist_ok = True)
 
 # Load model
@@ -95,7 +95,7 @@ while True:
             sequence_array = np.array(sequence)
 
             file_count = len(os.listdir(save_folder))
-            filename = f"file_{file_count}_distance_right.npy"
+            filename = f"file_{file_count}_default_left.npy"
             filepath = os.path.join(save_folder, filename)
 
             np.save(filepath, sequence_array)
